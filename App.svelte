8c6b8dc1f4604035cc8ccac6a245ebcf30e09e60
Annisa Puspitasari <nisa.codelifings@gmail.com>
2020-09-10 00:15:48 -0700

    Initial commit

---
+++ App.svelte
@@ -0,0 +1,19 @@
+<script>
+  import Button from "./Button.svelte";
+  import Navbar from "./Navbar.svelte";
+
+  let nama = "nisa";
+</script>
+
+<style>
+  main {
+    font-family: sans-serif;
+    text-align: center;
+  }
+</style>
+
+<main>
+<Navbar />
+<p>Lorem ipsum {nama} sit amet consectetur, adipisicing elit. Facere fugit, consectetur ea magni quidem nihil iste sed esse tempore, non enim aliquid pariatur eius itaque cumque quam magnam ullam maiores.</p>
+	<Button />
+</main>
---
+++ Navbar.svelte
@@ -0,0 +1,20 @@
+<script></script>
+
+<style>
+  :global(body) {
+    margin: 0;
+  }
+  nav {
+    background: #ff3e00;
+    padding: 10px 20px;
+  }
+  a {
+    text-decoration: none;
+    color: white;
+  }
+</style>
+<nav>
+ <a href="#">Dashboard</a>
+ <a href="#">Dashboard</a>
+ <a href="#">Dashboard</a>
+</nav>
