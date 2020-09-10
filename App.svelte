36573a0a687287ebf307087f6c6c81589950b3e6
Annisa Puspitasari <nisa.codelifings@gmail.com>
2020-09-10 00:24:13 -0700

    update

--- App.svelte
+++ App.svelte
@@ -10,10 +10,13 @@
     font-family: sans-serif;
     text-align: center;
   }
+  p {
+    margin: 20px;
+  }
 </style>
 
 <main>
 <Navbar />
-<p>Lorem ipsum {nama} sit amet consectetur, adipisicing elit. Facere fugit, consectetur ea magni quidem nihil iste sed esse tempore, non enim aliquid pariatur eius itaque cumque quam magnam ullam maiores.</p>
-	<Button />
+	
+<p>Lorem ipsum {nama} <Button /> sit amet consectetur, adipisicing elit. Facere fugit, consectetur ea magni quidem nihil iste sed esse tempore, non enim aliquid pariatur eius itaque cumque quam magnam ullam maiores.</p>
 </main>
